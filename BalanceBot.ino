5369845ec46ad90e31a43e59f6c14baa23e34c20
Alejandro Mendez <alejandro.mp95@hotmail.com>
2017-06-01 23:52:58 -0500

    modification to bt

---
+++ BalanceBot.ino
@@ -0,0 +1,177 @@
+
+#define MPU6500 1
+#define EMPL_TARGET_ARDUINO 1
+#define __AVR_ATmega2560__ 1
+#define UBRR1H 1
+#include <Arduino.h>
+#include <Source\PID\PID.h>
+#include <Source\I2Cdev\I2Cdev.h>
+#include <Source\helper3Dmath\helper_3dmath.h>
+#include <eMPL\inv_mpu.h>
+#include <eMPL\inv_mpu_dmp_motion_driver.h>
+#include <stdint.h>
+#include <HardwareSerial.h>
+#include <SoftwareSerial\src\SoftwareSerial.h>
+
+HardwareSerial BtPort = Serial1;//cambia el nombre del puerto serial
+
+const unsigned char signal_pin = 12;
+
+// Necesario para el MPU
+const char dmp_features =
+DMP_FEATURE_6X_LP_QUAT | DMP_FEATURE_SEND_CAL_GYRO |
+DMP_FEATURE_GYRO_CAL;
+unsigned const short mpu_rate_hz = 100;
+unsigned char sensors = // los sensores que se usaran y dosponibles en el modulo
+INV_XYZ_ACCEL | INV_XYZ_GYRO;
+//checar datasheet para ver que escalas y a que bits se debe asignar
+unsigned const char accel_fsr = 0;//es para la escala que se usara
+unsigned const char gyro_fsr = 0;//es para la escala que se usara
+uint8_t int_pin = 20;
+struct int_param_s int_param;
+
+char gyro_orientation[] = { 1, 0, 0,
+0, 1, 0,
+0, 0, 1 };
+
+
+//constantes del PID
+PID position;
+
+boolean dmpReady = 0;  // set true if DMP init was successful
+uint8_t mpuIntStatus;   // holds actual interrupt status byte from MPU
+uint8_t devStatus;      // return status after each device operation (0 = success, !0 = error)
+uint16_t packetSize;    // expected DMP packet size (default is 42 bytes)
+uint16_t fifoCount;     // count of all bytes currently in FIFO
+uint8_t fifoBuffer[64]; // FIFO storage buffer
+
+						// orientation/motion vars
+Quaternion q;           // [w, x, y, z]         quaternion container
+VectorFloat gravity;    // [x, y, z]            gravity vector
+int16_t gyro[3];        // [x, y, z]            gyro vector
+float ypr[3];           // [yaw, pitch, roll]   yaw/pitch/roll container and gravity vector
+
+
+int speed_real_l, speed_real_r;
+int pwm, pwm_l, pwm_r;
+int Turn_Need, Speed_Need;
+float angle, angular_rate;
+boolean blinkState = false;
+int rx_count = 0;
+byte buf_tmp = 0;
+uint8_t i2cData[14]; // Buffer for I2C data
+
+uint8_t setup_mpu(void)
+{
+	int_param.pin = int_pin;
+	mpu_init(&int_param);
+	mpu_set_gyro_fsr(gyro_fsr);
+	mpu_set_accel_fsr(accel_fsr);
+	//int mpu_set_lpf(unsigned short lpf) ;
+	mpu_set_sample_rate(mpu_rate_hz);
+	//int mpu_set_compass_sample_rate(unsigned short rate);
+	mpu_configure_fifo(sensors);
+	mpu_set_sensors(sensors);
+
+	dmp_load_motion_driver_firmware();
+	dmp_enable_feature(dmp_features);
+	dmp_set_fifo_rate(mpu_rate_hz);
+	dmp_set_interrupt_mode(DMP_INT_CONTINUOUS);
+	mpu_set_dmp_state(1);
+	dmp_set_interrupt_mode(DMP_INT_CONTINUOUS);
+
+	dmpReady = 1;
+	return 0;
+}
+
+//Interrupts
+volatile boolean mpuInterrupt = 0;     // indicates whether MPU interrupt pin has gone high
+void dmpDataReady()
+{
+	mpuInterrupt = 1;
+}
+
+volatile boolean btInterrupt = 0;
+void btDataReady()
+{
+	btInterrupt = 1;
+}
+
+
+void setup()
+{
+	BtPort.begin(9600);
+	Wire.begin();// join I2C bus (I2Cdev library doesn't do this automatically)
+	TWBR = 24; // 400kHz I2C clock (200kHz if CPU is 8MHz)
+			   //  Serial.begin(9600);
+
+			   // initialize device
+	if (setup_mpu())
+	{
+		Serial.println("mpu no inicializada correctamemte");
+	}
+
+	////////////////////////////////////////
+	// modificar deacuerdo al dispositivo que tengo despues de probarlo
+	// supply your own gyro offsets here, scaled for min sensitivity
+	
+							  // make sure it worked (returns 0 if so)
+	if (devStatus == 0)
+	{
+		// get expected DMP packet size for later comparison
+		packetSize = 0;
+	}
+	else
+	{
+		// ERROR!
+		// 1 = initial memory load failed
+		// 2 = DMP configuration updates failed
+		// (if it's going to break, usually the code will be 1)
+		Serial.print(("DMP Initialization failed (code "));
+		Serial.print(devStatus);
+		Serial.println((")"));
+	}
+}
+
+void loop()
+{
+
+	// if programming failed, don't try to do anything
+	if (!dmpReady) return;
+
+
+	// check for overflow (this should never happen unless our code is too inefficient)
+	if ((mpuIntStatus & 0x10) || fifoCount == 1024)
+	{
+		// reset so we can continue cleanly
+		Serial.println(F("FIFO overflow!"));
+		// otherwise, check for DMP data ready interrupt (this should happen frequently)
+	}
+	else if (mpuIntStatus & 0x02)
+	{
+
+	}
+	/* add main program code here */
+
+}
+
+uint8_t bt_recive(char* position)
+{
+	uint8_t data[2];
+	if (BtPort.available() > 0)
+	{
+		BtPort.readBytes(data, 2);
+	}
+	//aqui va el codigo para ver que es lo que va leyendo
+	//debe recivir el valor de dos variables x e y
+}
+
+uint8_t bt_config()
+{
+
+}
+
+uint8_t bt_init()
+{
+
+}
---
+++ Source/PID/PID.c
@@ -0,0 +1,137 @@
+#include "PID.h"
+#include "Types.h"
+#include "Utilities.h"
+
+#include <TermIO.h>
+#include <stdio.h>
+
+#define DEFAULT_ERROR 1.0f
+
+//push an error onto the previous error stack
+//and rotate all the previous errors
+void PID_push_prev_error(PID* this, float error){
+  int i;
+  float previous_error;
+  for(i=0;i<(MAX_PREV_ERRORS - 1);i++){
+    this->previous_errors[i+1] = this->previous_errors[i];
+  }
+  this->previous_errors[0]=error;
+}
+
+float PID_get_avg_error(PID* this){
+ int i;
+ float sum=0.0f;
+ for(i=0;i<MAX_PREV_ERRORS;i++){
+  sum += this->previous_errors[i];
+ }
+ return sum/((float)MAX_PREV_ERRORS);
+}
+
+float PID_get_prev_error(PID* this){
+ return this->previous_errors[0]; 
+}
+
+void PID_print_errors(PID* this){
+  int i;
+  printf("Errors:");
+  for(i=0;i<MAX_PREV_ERRORS;i++){
+    printf(" %f",this->previous_errors[i]);
+  }
+  printf("\n");
+}
+
+ //set all previous errors on the stack to DEFAULT_ERROR
+void PID_clear_prev_errors(PID* this){
+	int i;
+	for(i=0;i<MAX_PREV_ERRORS;i++){
+	  this->previous_errors[i] = DEFAULT_ERROR;
+	} 
+}
+
+void PID_init(PID* this, float Kp, float Ki, float Kd, float SS_threshold){
+	this->Kp = Kp;
+	this->Ki = Ki;
+	this->Kd = Kd;
+	this->setpoint = 0;
+	this->derivative = 0;
+	this->integral = 0;
+	
+	PID_clear_prev_errors(this);
+	
+	this->is_SS = 0;
+	this->SS_threshold = SS_threshold;
+}
+
+char PID_calc_steady_state(PID* this){
+    int i;
+    char above_threshold = 1;
+    for(i=0;i<MAX_PREV_ERRORS;i++){
+        if(abs_f(this->previous_errors[i]) > this->SS_threshold){
+            above_threshold = 0;
+        }
+    }
+    return above_threshold;
+}
+
+/*Should I be keeping track of dt in here?*/
+float PID_update(PID* this, float measured_value, float dt) {
+  float return_value = 0;
+  
+  //calculate difference between actual (measured) value
+  // and desired value (setpoint)
+	float error = this->setpoint - measured_value;
+	
+	// track error over time, scaled to the timer interval
+	//this->integral = this->integral + error*dt;
+	this->integral += error*dt;
+
+	//need to multiply numerator by 1000?
+  // determine the amount of change from the last time checked
+	this->derivative = (error - PID_get_prev_error(this))/dt;
+	PID_push_prev_error(this, error);
+	
+	
+	//calculate is steady state...
+	//printf("err: %f th: %f\n", abs_f(avg_error), this->SS_threshold);
+	if(PID_calc_steady_state(this)){
+		printf("is_SS\n");
+		this->is_SS = 1; 
+	}
+	
+	//calculate output value required to get to desired value
+	return_value += this->Kp * error;
+	return_value +=	this->Ki * this->integral;
+	return_value += this->Kd * this->derivative;
+	return return_value;
+}
+/*sensor input 0 -> 426*/
+
+void PID_set_setpoint(PID* this, float setpoint) {
+	this->setpoint = setpoint;
+	this->is_SS = 0;
+	PID_clear_prev_errors(this);
+}
+
+float PID_get_Kp(PID* this){
+  return this->Kp;
+}
+
+void PID_set_Kp(PID* this, float Kp){
+  this->Kp = Kp;
+}
+void PID_set_Ki(PID* this, float Ki){
+  this->Ki = Ki;
+}
+void PID_set_Kd(PID* this, float Kd){
+  this->Kd;
+}
+
+void PID_set_SS_threshold(PID* this, float SS_threshold){
+  this->SS_threshold = SS_threshold;
+}
+
+//returns a boolean depending on whether the PID is in a steady state
+char PID_is_steady_state(PID* this){
+  return this->is_SS;
+}
+  
